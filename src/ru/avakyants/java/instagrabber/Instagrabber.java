2725113abdcc304cc84de53d4017cfc4ef1d6e82
tema14 <tema14@172.20.10.2>
2018-02-15 08:34:30 +0300

    print account user login after download

--- src/ru/avakyants/java/instagrabber/Instagrabber.java
+++ src/ru/avakyants/java/instagrabber/Instagrabber.java
@@ -13,6 +13,15 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import me.postaddict.instagram.scraper.Instagram;
+import me.postaddict.instagram.scraper.cookie.CookieHashSet;
+import me.postaddict.instagram.scraper.cookie.DefaultCookieJar;
+import me.postaddict.instagram.scraper.interceptor.ErrorInterceptor;
+import me.postaddict.instagram.scraper.model.Media;
+import okhttp3.CookieJar;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
 public class Instagrabber {
 
 	public static void main(String[] args) {
@@ -23,7 +32,8 @@ public class Instagrabber {
 			
 			Instagrabber instgrb = new Instagrabber();					
 			List<String> results = instgrb.findAndReturnList(instgrb.getSourceByURL(args[1]),"<meta property=\"og:image\" content=\"(.+)\" />");
-			instgrb.saveToFiles(results, args[0]);			
+			instgrb.saveToFiles(results, args[0]);
+			instgrb.printAccountInfoByURL(args[1]);
 			
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -75,6 +85,23 @@ public class Instagrabber {
 				System.out.println("Successfuly download image: "+imageURL+" to: "+path);
 			}
 		}
+	}
+	
+	public void printAccountInfoByURL(String URL) throws IOException {
+		HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+		loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+		OkHttpClient httpClient = new OkHttpClient.Builder()
+		        .addNetworkInterceptor(loggingInterceptor)
+		        .addInterceptor(new ErrorInterceptor())
+		        .cookieJar((CookieJar) new DefaultCookieJar(new CookieHashSet()))
+		        .build();
+		
+		Instagram instagram = new Instagram(httpClient);
+		
+		/*Get media by url*/		
+		Media media = instagram.getMediaByUrl(URL);
+		System.out.println("Username: "+media.getOwner().getUsername());
 	}
 
 }
